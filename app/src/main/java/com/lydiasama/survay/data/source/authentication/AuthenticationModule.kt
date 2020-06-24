5b3c3d65b3619c441a7380c6e3c21c60d5c3b4e5
lydiasama <hallelujah.lydia@gmail.com>
2020-06-24 22:17:33 +0700

    implement call service refreshToken

--- app/src/main/java/com/lydiasama/survay/data/source/authentication/AuthenticationModule.kt
+++ app/src/main/java/com/lydiasama/survay/data/source/authentication/AuthenticationModule.kt
@@ -1,11 +1,13 @@
 package com.lydiasama.survay.data.source.authentication
 
-import org.koin.core.qualifier.named
+import com.lydiasama.survay.data.AuthenticationResult
+import io.reactivex.functions.Function
 import org.koin.dsl.module
+import retrofit2.Response
+import retrofit2.Retrofit
 
 val authenticationModule = module {
-//	scope(named<AuthenticationService>()) {
-//		scope<AuthenticationApi> { AuthenticationApi }
-//		scope<> {  }
-//	}
+	factory<AuthenticationApi> { get<Retrofit>().create(AuthenticationApi::class.java) }
+	factory<Function<Response<AuthenticationResult>, AuthenticationResult>> { AuthenticationMapper() }
+	factory<AuthenticationDataSource> { AuthenticationService(get(), get()) }
 }
--- app/src/main/java/com/lydiasama/survay/network/TokenAuthenticator.kt
+++ app/src/main/java/com/lydiasama/survay/network/TokenAuthenticator.kt
@@ -1,20 +1,26 @@
 package com.lydiasama.survay.network
 
+import android.util.Log
+import com.lydiasama.survay.data.source.authentication.AuthenticationService
 import okhttp3.Authenticator
 import okhttp3.Request
 import okhttp3.Response
 import okhttp3.Route
+import org.koin.java.KoinJavaComponent.inject
 
 class TokenAuthenticator : Authenticator {
 	override fun authenticate(route: Route?, response: Response): Request? {
 		val updatedToken = getNewToken()
+		Log.d("updatedToken", updatedToken)
 		return response.request.newBuilder()
 				.header("Authorization", updatedToken)
 				.build()
 	}
 
 	private fun getNewToken(): String {
-		// TODO Call authen usecase here and save to Shared pref
-		return ""
+		val authenticationService: AuthenticationService by inject(
+				AuthenticationService::class.java)
+		return authenticationService.getAccessToken()
+				.blockingFirst()
 	}
 }
